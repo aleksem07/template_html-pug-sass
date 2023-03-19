047dffc6bb29e2ea4673cd206431dda36c993a05
aleksem07 <aleksem07@gmail.com>
2023-03-19 13:06:12 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,2 @@
+
+import'./menuToggle.js';console.log('1');
---
+++ js/menuToggle.js
@@ -0,0 +1,2 @@
+
+(()=>{const menuButton=document.querySelector(".menu-nav__button");const menuList=document.querySelector(".menu-nav__list");menuButton.addEventListener("click",()=>{let expanded=menuButton.getAttribute("aria-expanded")==="true";menuButton.setAttribute("aria-expanded",!expanded);menuButton.classList.toggle("menu__button--open");menuList.classList.toggle("menu__list--open");});})();
